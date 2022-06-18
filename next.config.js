9d4b7a9a124182327fb8f5c129baec06899dd7e9
samaneh karimi <samanehkarimi17@gmail.com>
2022-06-18 17:34:25 +0430

    image config

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
+// next.config.js
+
+module.exports = {
+    images: {
+        loader: "imgix",
+        path: "https://noop/",
+      }
+  }
